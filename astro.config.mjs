b21e747efd1e7420fcdf9120034ad8466f5d7a37
mide73924 <micheldeosaran@proton.me>
2026-02-13 23:05:34 -0500

    Update base astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mide73924.github.io/Data-Structures",
+  base: "/Data-Structures/",
   // base: '/micheldeo/',  // <--- this ensures all built assets use the correct subpath
 
   vite: {
